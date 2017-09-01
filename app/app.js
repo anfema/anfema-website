506e971f115d6c9585efa3585f521d124b132614
radofo <r.adofo@anfe.ma>
2017-09-01 16:24:13 +0200

    Initialize Project
    Re #1

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ import config from './config/environment';
 
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
+
+
   podModulePrefix: config.podModulePrefix,
   Resolver
 });
