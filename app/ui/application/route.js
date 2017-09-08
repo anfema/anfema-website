bb37ce988081906777e523008bce7f9552498a11
radofo <r.adofo@anfe.ma>
2017-09-08 15:52:33 +0200

    REFACTOR: Rename `inject` to `service`
    Specify the injected helper as `service`
    
    Re #17

--- app/ui/application/route.js
+++ app/ui/application/route.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
-	intl: inject(),
+	intl: service(),
 
 	beforeModel() {
 		return this.get('intl').setLocale('en-us');
