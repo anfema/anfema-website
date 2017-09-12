7c960099709782731f53505389c7976b0582a8df
Philipp Grieser <philippgrieser@gmail.com>
2017-09-12 16:05:39 +0200

    FEATURE: Projects Item Component
    add project-item component (extend link-to component)
    
    Re #26

---
+++ tests/integration/ui/components/project-item/component-test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describe('Integration | Component | project item', function () {
+	setupComponentTest('project-item', {
+		integration: true,
+	});
+
+	it('renders', function () {
+		// Set any properties with this.set('myProperty', 'value');
+		// Handle any actions with this.on('myAction', function (val) { ... });
+		// Template block usage:
+		// this.render(hbs`
+		// 	{{#project-item}}
+		// 		template content
+		// 	{{/project-item}}
+		// `);
+
+		this.render(hbs`{{project-item 'projects.details' 'bmw7series'}}`);
+
+		expect(this.$()).to.have.length(1);
+	});
+});
