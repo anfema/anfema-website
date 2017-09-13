1ac0ad8990108025d67a86e350ce83c142822ba8
Philipp Grieser <philippgrieser@gmail.com>
2017-09-13 18:56:01 +0200

    FEATURE: Services slider element for the landing page
    Add services-slider component and integration tests
    
    Re #39

--- app/ui/index/route.js
+++ app/ui/index/route.js
@@ -1,4 +1,38 @@
 import Route from '@ember/routing/route';
 import PrefixMixin from 'anfema/mixins/prefix-mixin';
 
-export default Route.extend(PrefixMixin, {});
+export default Route.extend(PrefixMixin, {
+	prefix: null,
+
+	model() {
+		return {
+			prefix: this.get('prefix'),
+			services: {
+				concept: {
+					title: 'concept title',
+					text: 'concept text',
+				},
+				design: {
+					title: 'design title',
+					text: 'design text',
+				},
+				development: {
+					title: 'development title',
+					text: 'development text',
+				},
+				consulting: {
+					title: 'consulting title',
+					text: 'consulting text',
+				},
+				support: {
+					title: 'support title',
+					text: 'support text',
+				},
+				management: {
+					title: 'management title',
+					text: 'management text',
+				},
+			},
+		};
+	},
+});
