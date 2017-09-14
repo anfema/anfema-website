2ff1933122e403afdaeac37ea0242336955c557e
Philipp Grieser <philippgrieser@gmail.com>
2017-09-14 15:51:01 +0200

    REFACTOR: change service-slider to content-slider
    Change slider to support all kind of content & add acceptance test
    
    Re #30

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -2,6 +2,7 @@ import { afterEach, beforeEach, describe, it } from 'mocha';
 import { expect } from 'chai';
 import startApp from 'anfema/tests/helpers/start-app';
 import destroyApp from 'anfema/tests/helpers/destroy-app';
+import { click, find, findAll } from 'ember-native-dom-helpers';
 
 describe('Acceptance | index', function () {
 	let application;
@@ -19,8 +20,37 @@ describe('Acceptance | index', function () {
 		expect(currentURL()).to.equal('/');
 	});
 
-	it('can visit /imprint from footer', async function () {
-		await click('[data-test-footer-imprint]');
-		expect(find('[data-test-imprint-page]')).to.exist;
+	it('renders content items correctly', async function () {
+		// all content items visible
+		expect(find('[data-test-content-link-concept]')).to.exist;
+		expect(find('[data-test-content-link-design ]')).to.exist;
+		expect(find('[data-test-content-link-development]')).to.exist;
+		expect(find('[data-test-content-link-consulting]')).to.exist;
+		expect(find('[data-test-content-link-support]')).to.exist;
+		expect(find('[data-test-content-link-management]')).to.exist;
+
+		let contentItems = await findAll('[data-test-content-item]');
+		let contentItemsHidden = await findAll('[data-test-content-item][hidden]');
+
+		// just one item visible
+		contentItems = contentItems.filter(item => contentItemsHidden.indexOf(item) < 0);
+		assert(contentItems.length === 1);
+
+		// standard content
+		assert(contentItems[0].getElementsByTagName('h1')[0].innerText === 'concept title');
+		assert(contentItems[0].getElementsByTagName('p')[0].innerText === 'concept text');
+
+		click(find('[data-test-content-link-design]'));
+
+		contentItems = await findAll('[data-test-content-item]');
+		contentItemsHidden = await findAll('[data-test-content-item][hidden]');
+
+		// just one item visible
+		contentItems = contentItems.filter(item => contentItemsHidden.indexOf(item) < 0);
+		assert(contentItems.length === 1);
+
+		// new content
+		assert(contentItems[0].getElementsByTagName('h1')[0].innerText === 'design title');
+		assert(contentItems[0].getElementsByTagName('p')[0].innerText === 'design text');
 	});
 });
---
+++ tests/integration/ui/components/content-slider/component-test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
+
+describe('Integration | Component | content slider', function () {
+	setupComponentTest('content-slider', {
+		integration: true,
+	});
+
+	beforeEach(function () {
+		this.set('content', [{
+			id: 'concept',
+			title: 'concept title',
+			text: 'concept text',
+			hidden: false,
+		}]);
+	});
+
+	it('renders', function () {
+		this.render(hbs`
+			{{content-slider content=content param=''}}
+		`);
+		expect(find('[data-test-content-item]')).to.exist;
+	});
+});
