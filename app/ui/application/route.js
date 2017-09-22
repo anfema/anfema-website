f3a91f56dbd8870ae2fcf3c76383740ef0a69a3b
radofo <r.adofo@anfe.ma>
2017-09-22 18:38:24 +0200

    FEATURE: Update yaml files
    Update content in yaml files and fetch generated jsons in index model hook
    
    Re #59

--- app/ui/application/route.js
+++ app/ui/application/route.js
@@ -1,11 +1,11 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import {inject as service} from '@ember/service';
 
 export default Route.extend({
 	intl: service(),
 
 	beforeModel() {
 		// TODO: Define default language by browser header / navigator (must work in Fastboot as well)
-		return this.get('intl').setLocale('de');
+		return this.get('intl').setLocale('en-us');
 	},
 });
