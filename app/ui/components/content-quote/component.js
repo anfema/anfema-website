0e2f1f62cb0325ed20943fdf41d3b0e4f2e66f82
Sven ten Pas <s.tenpas@anfe.ma>
2017-09-22 18:53:40 +0200

    FEATURE: Content Quote component
    - move content in component invocation
    - grid layout
    - styling
    
    Re #60

--- app/ui/components/content-quote/component.js
+++ app/ui/components/content-quote/component.js
@@ -4,8 +4,8 @@ export default Component.extend({
 	tagName: 'section',
 	classNames: ['content-quote'],
 
-	quote_text: null,
-	quote_source: null,
-	project_id: null,
-	project_link_label: null,
+	quoteText: null,
+	quoteSource: null,
+	projectId: null,
+	projectLinkLabel: null,
 });
