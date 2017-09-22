a8298107b3e0af05c87443bb04acf613c5fde0d7
Florian Pichler <f.pichler@anfe.ma>
2017-09-22 15:36:45 +0200

    FEATURE: Move cover page to component
    Re #49

---
+++ tests/integration/ui/components/content-lp-cover/component-test.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
+
+describe('Integration | Component | content lp cover', function() {
+	setupComponentTest('content-lp-cover', {
+		integration: true,
+	});
+
+	it('renders', function() {
+		// Set any properties with this.set('myProperty', 'value');
+		// Handle any actions with this.on('myAction', function (val) { ... });
+		// Template block usage:
+		// this.render(hbs`
+		// 	{{#content-lp-cover}}
+		// 		template content
+		// 	{{/content-lp-cover}}
+		// `);
+
+		this.render(hbs`{{content-lp-cover headline="foo" subtitle="<p>bar</p>"}}`);
+
+		expect(find('.content-lp-cover')).to.exist;
+		expect(find('.content-lp-cover h1')).to.exist;
+		expect(find('.content-lp-cover h1').innerText).to.equal('foo');
+		expect(find('.content-lp-cover p')).to.exist;
+		expect(find('.content-lp-cover p').innerText).to.equal('bar');
+	});
+});
