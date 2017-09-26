64b548b2f3481bcad79a88bda4a6584435de5f8d
Sven ten Pas <s.tenpas@anfe.ma>
2017-09-26 14:54:57 +0200

    STYLE: Linter errors
    - StyleLint
    - ESLint
    - Prettier
    
    Re #60
    
    Re #60

--- tests/integration/ui/components/content-quote/component-test.js
+++ tests/integration/ui/components/content-quote/component-test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
 
 describe('Integration | Component | content quote', function() {
 	setupComponentTest('content-quote', {
