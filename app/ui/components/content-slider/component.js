2149520ac9f55353dc5e9267c0c8dff086cd8c13
André Wachter <a.wachter@anfe.ma>
2017-09-29 16:09:12 +0200

    REFACTOR: Use underscore for private attributes
    Re: 30

--- app/ui/components/content-slider/component.js
+++ app/ui/components/content-slider/component.js
@@ -18,6 +18,8 @@ export default Component.extend({
 	selected: null,
 	/** @type {boolean} */
 	cycle: true,
+	/** @type {number} */
+	slideDirection: 0,
 
 	onPrevious: undefined,
 	onNext: undefined,
@@ -27,72 +29,64 @@ export default Component.extend({
 	 */
 
 	/** @type {number | undefined} */
-	dragStartPosition: undefined,
+	_dragStartPosition: undefined,
 	/** @type {number} */
-	slideDirection: 0,
-	/** @type {number} */
-	dragOffset: 0,
+	_dragOffset: 0,
 	/** @type {boolean} */
-	isTransitioning: false,
+	_isTransitioning: false,
 
 	/** @returns {number} */
-	currentIndex: computed('data.@each', 'selected', function () {
-		return this.get('data').findIndex(data => data === this.get('selected'));
+	_currentIndex: computed('data.@each', 'selected', function () {
+		return this.data.findIndex(data => data === this.selected);
 	}),
 
 	/** @returns {number} */
-	logoStep: computed('data.@each', 'selected', function() {
-		return this.get('currentSlide').logoStep;
+	_logoStep: computed('data.@each', 'selected', function() {
+		return this.get('_currentSlide').logoStep;
 	}),
 
 	/** @returns {boolean} */
-	previousDisabled: computed('cycle', 'currentIndex', 'isTransitioning', function() {
-		const currentIndex = this.get('currentIndex');
+	_previousDisabled: computed('cycle', '_currentIndex', '_isTransitioning', function() {
+		const currentIndex = this.get('_currentIndex');
 
-		return (!this.cycle && currentIndex < 0) || this.isTransitioning;
+		return (!this.cycle && currentIndex < 0) || this._isTransitioning;
 	}),
 
 	/** @returns {boolean} */
-	nextDisabled: computed('cycle', 'data', 'currentIndex', 'isTransitioning', function() {
-		const currentIndex = this.get('currentIndex');
+	_nextDisabled: computed('cycle', 'data', '_currentIndex', '_isTransitioning', function() {
+		const currentIndex = this.get('_currentIndex');
 
-		return (!this.cycle && currentIndex > this.data.length - 1) || this.isTransitioning;
+		return (!this.cycle && currentIndex > this.data.length - 1) || this._isTransitioning;
 	}),
 
 	/** @returns {object} */
-	previousSlide: computed('cycle', 'data.@each', 'currentIndex', function() {
-		const currentIndex = this.get('currentIndex');
+	_previousSlide: computed('cycle', 'data.@each', '_currentIndex', function() {
+		const currentIndex = this.get('_currentIndex');
 
-		if (this.cycle || currentIndex > 0) {
-			return this.data[ mod(currentIndex - 1, this.data.length) ];
-		}
-		else {
-			return undefined;
-		}
+		return this.cycle || currentIndex > 0
+			? this.data[ mod(currentIndex - 1, this.data.length) ]
+			: undefined;
 	}),
 
 	/** @returns {object} */
-	currentSlide: computed('data.@each', 'selected', function () {
-		return this.get('data').find(data => data === this.get('selected'));
+	_currentSlide: computed('data.@each', 'selected', function () {
+		return this.data.find(data => data === this.selected);
 	}),
 
 	/** @returns {object} */
-	nextSlide: computed('cycle', 'data.@each', 'currentIndex', function() {
-		const currentIndex = this.get('currentIndex');
+	_nextSlide: computed('cycle', 'data.@each', '_currentIndex', function() {
+		const _currentIndex = this.get('_currentIndex');
 
-		if (this.cycle || currentIndex < this.data.length - 1) {
-			return this.data[mod(currentIndex + 1, this.data.length) ];
-		}
-		else {
-			return undefined;
-		}
+		return this.cycle || _currentIndex < this.data.length - 1
+			? this.data[mod(_currentIndex + 1, this.data.length) ]
+			: undefined;
 	}),
 
 	/** @returns {number} */
-	sliderStyle: computed('selected', 'dragOffset', 'dragStartPosition', 'slideDirection', function () {
-		const userIsInteracting = this.dragStartPosition !== undefined;
+	_sliderStyle: computed('selected', '_dragOffset', '_dragStartPosition', 'slideDirection', function () {
+		const userIsInteracting = this._dragStartPosition !== undefined;
 		const initTransition = this.slideDirection !== 0;
-		const wasDragging = this.dragOffset !== 0 && !userIsInteracting;
+		const wasDragging = this._dragOffset !== 0 && !userIsInteracting;
 
 		//  0: right slide
 		// -1: middle slide
@@ -104,40 +98,40 @@ export default Component.extend({
 			Ember.run.next(this, () => {
 				this.setProperties({
 					slideDirection: 0,
-					dragStartPosition: undefined,
-					dragOffset: 0,
+					_dragStartPosition: undefined,
+					_dragOffset: 0,
 				});
 			});
 		}
 
 		return htmlSafe(
-			`transform: translate(calc(${fullOffset}% + ${this.dragOffset}px));` +
+			`transform: translate(calc(${fullOffset}% + ${this._dragOffset}px));` +
 			// disable transition while user is interacting
 			(initTransition || userIsInteracting ? 'transition: none;' : '')
 		);
 	}),
 
-	sliderExtraClass: computed('dragStartPosition', 'isTransitioning', function () {
-		const userIsInteracting = this.dragStartPosition !== undefined;
+	_sliderExtraClass: computed('_dragStartPosition', '_isTransitioning', function () {
+		const userIsInteracting = this._dragStartPosition !== undefined;
 
-		return userIsInteracting || this.isTransitioning
+		return userIsInteracting || this._isTransitioning
 			? 'content-slider__slider__transitioning'
 			: 'content-slider__slider__steady';
 	}),
 
 	actions: {
 		slideToPrevious() {
-			const currentIndex = this.get('currentIndex');
+			const currentIndex = this.get('_currentIndex');
 
-			if (this.onPrevious && (this.cycle || this.currentIndex > 0)) {
+			if (this.onPrevious && (this.cycle || this._currentIndex > 0)) {
 				this.onPrevious(this.data[mod(currentIndex - 1, this.data.length)]);
 			}
 		},
 
 		slideToNext() {
-			const currentIndex = this.get('currentIndex');
+			const currentIndex = this.get('_currentIndex');
 
-			if (this.onNext && (this.cycle || this.currentIndex < this.data.length - 1)) {
+			if (this.onNext && (this.cycle || this._currentIndex < this.data.length - 1)) {
 				this.onNext(this.data[mod(currentIndex + 1, this.data.length)]);
 			}
 		},
@@ -151,7 +145,7 @@ export default Component.extend({
 		this._super(...arguments)
 		this.oldAttrs = [];
 		this.onTransitionEnd = () => {
-			this.set('isTransitioning', false);
+			this.set('_isTransitioning', false);
 		}
 	},
 
@@ -197,34 +191,34 @@ export default Component.extend({
 
 	/** @argument startPos {number} */
 	startDrag(startPos) {
-		if (this.dragStartPosition === undefined) {
+		if (this._dragStartPosition === undefined) {
 			this.setProperties({
-				dragStartPosition: startPos,
-				dragOffset: 0
+				_dragStartPosition: startPos,
+				_dragOffset: 0
 			});
 		}
 	},
 
 	endDrag() {
-		if (this.dragStartPosition !== undefined) {
-			const offset = this.get('dragOffset');
-			const currentIndex = this.get('currentIndex');
+		if (this._dragStartPosition !== undefined) {
+			const offset = this.get('_dragOffset');
+			const _currentIndex = this.get('_currentIndex');
 			const sliderWidth = this.element.querySelector('.content-slider__slidewindow').clientWidth;
 
 			if (Math.abs(offset) / sliderWidth > 0.3) {
-				if (this.onPrevious && offset > 0 && (this.cycle || currentIndex > 0)) {
-					this.onPrevious(this.data[mod(currentIndex - 1, this.data.length)]);
+				if (this.onPrevious && offset > 0 && (this.cycle || _currentIndex > 0)) {
+					this.onPrevious(this.data[mod(_currentIndex - 1, this.data.length)]);
 				}
-				else if (this.onNext && offset < 0 && (this.cycle || currentIndex < this.data.length - 1)) {
-					this.onNext(this.data[mod(currentIndex + 1, this.data.length)]);
+				else if (this.onNext && offset < 0 && (this.cycle || _currentIndex < this.data.length - 1)) {
+					this.onNext(this.data[mod(_currentIndex + 1, this.data.length)]);
 				}
 				this.setProperties({
-					dragStartPosition: undefined,
+					_dragStartPosition: undefined,
 				});
 			}
 			else {
 				this.setProperties({
-					dragStartPosition: undefined,
+					_dragStartPosition: undefined,
 				});
 			}
 		}
@@ -235,8 +229,8 @@ export default Component.extend({
 	 * @argument newPos {number}
 	*/
 	updateDrag(newPos) {
-		if (this.dragStartPosition !== undefined && this.slideDirection === 0) {
-			this.set('dragOffset', newPos - this.dragStartPosition);
+		if (this._dragStartPosition !== undefined && this.slideDirection === 0) {
+			this.set('_dragOffset', newPos - this._dragStartPosition);
 		}
 	},
 });
