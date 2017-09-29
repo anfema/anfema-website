d66bf659227c90d68ba3e8d85b359ceb5075c2de
Florian Pichler <f.pichler@anfe.ma>
2017-09-29 15:03:19 +0200

    REFACTOR: Use router service to change language
    Re #82

--- app/ui/index/route.js
+++ app/ui/index/route.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
-import PrefixMixin from 'anfema/mixins/prefix-mixin';
+import { inject as service } from '@ember/service';
 
-export default Route.extend(PrefixMixin, {});
+export default Route.extend({
+	intl: service(),
+
+	beforeModel() {
+		this.replaceWith('language', this.get('intl.locale.0')); // 'de' is our default language, we could try to detect something here
+	},
+});
--- app/ui/language/route.js
+++ app/ui/language/route.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
-import PrefixMixin from 'anfema/mixins/prefix-mixin';
+import { inject as service } from '@ember/service';
 
-export default Route.extend(PrefixMixin, {});
+export default Route.extend({
+	intl: service(),
+
+	beforeModel(transition) {
+		return this.get('intl').setLocale(transition.params.language.language_id);
+	},
+
+	model() {},
+});
