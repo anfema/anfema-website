22f4522e5adcbcb98053d4fac1a9070c86b1c120
Florian Pichler <f.pichler@anfe.ma>
2017-09-29 11:40:23 +0200

    REFACTOR: Prefer using Ember for DOM manipulation
    Re #32

--- app/ui/components/content-folder-item/component.js
+++ app/ui/components/content-folder-item/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { next } from '@ember/runloop';
 
 export default Component.extend({
 	classNameBindings: [':content-folder-item', 'isActive:content-folder-item--active'],
@@ -11,77 +12,44 @@ export default Component.extend({
 		return this.get('content.id') === this.get('selected');
 	}),
 
+	didReceiveAttrs() {
+		this._setContentHeight();
+
+		if (!this.get('isActive')) {
+			// Wait one tick, then call setContentHeight again, this time resetting the style
+			next(this, '_setContentHeight', null);
+		}
+	},
+
 	didInsertElement() {
-		//console.log('inserted content-folder-item');
+		this._content = this.element.querySelector('.content-folder-item__content');
 
-		function collapseContent(element) {
-			// get the height of the element's inner content, regardless of its actual size
-			var contentHeight = element.scrollHeight;
+		this._contentTransitionEnd = event => {
+			if (this.get('isActive')) {
+				this._setContentHeight('auto');
+			}
+		};
 
-			// temporarily disable all css transitions
-			var elementTransition = element.style.transition;
-			element.style.transition = '';
+		this._content.addEventListener('transitionend', this._contentTransitionEnd);
+	},
 
-			// on the next frame (as soon as the previous style change has taken effect),
-			// explicitly set the element's height to its current pixel height, so we
-			// aren't transitioning out of 'auto'
-			requestAnimationFrame(function() {
-				element.style.height = contentHeight + 'px';
-				element.style.transition = elementTransition;
+	willRemoveElement() {
+		this._content.removeEventListener('transitionend', this._contentTransitionEnd);
+	},
 
-				// on the next frame (as soon as the previous style change has taken effect),
-				// have the element transition to height: 0
-				requestAnimationFrame(function() {
-					element.style.height = 0 + 'px';
-				});
-			});
-
-			// mark the content as "currently collapsed"
-			element.setAttribute('data-collapsed', 'true');
+	_setContentHeight(height = true) {
+		if (!this._content) {
+			return;
 		}
 
-		function expandContent(element) {
-			// get the height of the element's inner content, regardless of its actual size
-			var contentHeight = element.scrollHeight;
-
-			// have the element transition to the height of its inner content
-			element.style.height = contentHeight + 'px';
-
-			// when the next css transition finishes (which should be the one we just triggered)
-			var rmListener = function(e) {
-				element.removeEventListener('transitioned', rmListener, false);
-			}
-
-			element.addEventListener('transitionend', rmListener, function(e) {
-				// remove this event listener so it only gets triggered once
-				// element.removeEventListener('transitionend', arguments.callee);
-
-				// remove "height" from the element's inline styles, so it can return to its initial value
-				element.style.height = null;
-			});
-
-			/* var foo = function(e) {
-				"use strict";
-				console.log(e);
-				document.removeEventListener('keyup', foo, false);
-			}
-
-			document.addEventListener('keyup', foo); */
-
-			// mark the content as "currently not collapsed"
-			element.setAttribute('data-collapsed', 'false');
+		if (height === true) {
+			height = this._content.scrollHeight;
 		}
 
-		document.querySelector('.content-folder__navigation-link').addEventListener('click', function(e) {
-			var content = document.querySelector('.content-folder__content');
-			var isCollapsed = content.getAttribute('data-collapsed') === 'true';
+		if (typeof height === 'number') {
+			height = `${height}px`;
+		}
 
-			if(isCollapsed) {
-				expandContent(content)
-				content.setAttribute('data-collapsed', 'false')
-			} else {
-				collapseContent(content)
-			}
-		});
+		this._content.style.height = height;
 	},
 });
