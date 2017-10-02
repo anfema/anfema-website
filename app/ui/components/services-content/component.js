1e3c6c62914c1cbcade5bb307eb6623cf8dfab05
André Wachter <a.wachter@anfe.ma>
2017-10-02 11:30:06 +0200

    REFACTOR: Add onChange event listener and calc slide direction for slider
    Re: 30

--- app/ui/components/services-content/component.js
+++ app/ui/components/services-content/component.js
@@ -5,37 +5,51 @@ export default Component.extend({
 	classNames: ['service-content'],
 
 	data: undefined,
-
 	selectedId: undefined,
 
-	slideDirection: 0,
-
-	selected: computed('selectedId', function() {
+	_slideDirection: 0,
+	_selected: computed('data.@each', 'selectedId', function() {
 		return this.selectedId
 			? this.data.find(service => service.id === this.selectedId)
 			: this.data[0];
 	}),
-
-	logoStep: computed('data', 'selectedId', function() {
-		const currentId = this.get('selectedId');
-		const currentService = this.get('data').find(service => service.id === currentId);
+	_logoStep: computed('data.@each', 'selectedId', function() {
+		const currentService = this.get('data').find(service => service.id === this.selectedId);
 
 		return currentService ? currentService.logoStep : 0;
 	}),
+
+	_oldSelectedId: undefined,
 
 	actions: {
 		previousSlide(slide) {
 			this.setProperties({
 				selectedId: slide.id,
-				slideDirection: -1
+				_slideDirection: -1
 			});
 		},
 
 		nextSlide(slide) {
 			this.setProperties({
 				selectedId: slide.id,
-				slideDirection: 1
+				_slideDirection: 1
+			});
+		},
+
+		itemSelected(slide) {
+			const oldIndex = this.data.findIndex(item => item === this.get('_selected'));
+			const newIndex = this.data.findIndex(item => item === slide);
+			const direction = oldIndex < newIndex ? 1 : -1;
+
+			this.setProperties({
+				selectedId: slide.id,
+				_slideDirection: direction
 			});
 		}
+	},
+
+	didReceiveAttrs() {
+		this._super(...arguments);
+		this._oldSelectedId = this.selectedId;
 	}
 });
