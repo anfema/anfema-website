d6ba94fb261cd26ff526ead3f8e58bd6621f9401
André Wachter <a.wachter@anfe.ma>
2017-10-05 10:29:48 +0200

    BUGFIX: Fix "services-content" naming inconsistencies

--- app/ui/components/services-content/component.js
+++ app/ui/components/services-content/component.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-	classNames: ['service-content'],
+	classNames: ['services-content'],
 
 	data: undefined,
 	selectedId: undefined,
