f336293c5db036a85583951ee33dfbdba7abd1e7
André Wachter <a.wachter@anfe.ma>
2017-10-06 11:54:58 +0200

    REFACTOR: Enable dragging and automatic centering of "menu-strip" component
    Re: #121

--- app/ui/components/menu-strip/component.js
+++ app/ui/components/menu-strip/component.js
@@ -1,20 +1,140 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import Ember from 'ember';
+import { htmlSafe } from '@ember/string';
 
 export default Component.extend({
 	classNames: ['menu-strip'],
+	classNameBindings: ['isInteracting'],
 
 	/** @type {object[]} */
 	items: null,
 	/** @type {object} */
 	selected: undefined,
-
 	onChange: undefined,
+
+	/** @type {number | undefined} */
+	_dragStartPosition: undefined,
+	_dragOffset: 0,
+	/** @type {number} */
+	_scrollOffset: 0,
+	/** @type {number} */
+	_scrollPosition: 0,
+	/** @type {boolean} */
+	_isTransitioning: false,
+	_oldSelected: undefined,
+
+	isInteracting: computed('_dragStartPosition', function () {
+		return this._dragStartPosition !== undefined;
+	}),
 
 	actions: {
 		onItemSelected(item) {
-			if (this.onChange && this.selected !== item) {
+			// Firefox generates click events even after dragging, so
+			// only call callback, when we did not drag.
+			if (this._dragOffset === 0 && this.onChange && this.selected !== item) {
 				this.onChange(item);
 			}
-		},
+		}
+	},
+
+	init() {
+		this._super(...arguments)
+		this.oldAttrs = [];
+	},
+
+	didReceiveAttrs() {
+		this._super(...arguments);
+		if (this._oldSelected !== this.selected) {
+			this.scrollToSelectedItem();
+		}
+		this._oldSelected = this.selected;
+	},
+
+	didInsertElement() {
+		this.scrollToSelectedItem();
+	},
+
+	/*
+	 * event listeners
+	 */
+	touchStart(e) {
+		this.startDrag(e.touches[0].clientX);
+	},
+	touchEnd(e) {
+		this.endDrag();
+	},
+	touchMove(e) {
+		this.updateDrag(e.touches[0].clientX)
+	},
+	mouseDown(e) {
+		this.startDrag(e.clientX);
+	},
+	mouseUp(e) {
+		this.endDrag();
+	},
+	mouseLeave(e) {
+		this.endDrag();
+	},
+	mouseMove(e) {
+		this.updateDrag(e.clientX);
+	},
+	dragStart(e) {
+		return false;
+	},
+
+	/*
+	 * private methods
+	 */
+
+	startDrag(startPos) {
+		if (this._dragStartPosition === undefined) {
+			this.setProperties({
+				_dragStartPosition: startPos,
+				_dragOffset: 0,
+				_scrollPosition: this.element.scrollLeft,
+			});
+		}
+	},
+
+	endDrag() {
+		if (this._dragStartPosition !== undefined) {
+			this.setProperties({
+				_dragStartPosition: undefined,
+				_scrollPosition: 0
+			});
+		}
+	},
+
+	/**
+	 * @argument newPos {number}
+	 */
+	updateDrag(newPos) {
+		if (this._dragStartPosition !== undefined) {
+			const offset = this._dragStartPosition - newPos;
+
+			this.setProperties({
+				_dragOffset: offset,
+			})
+
+			this.element.scrollLeft = this._scrollPosition + offset;
+		}
+	},
+
+	scrollToSelectedItem() {
+		const index = this.items.findIndex(item => item === this.selected);
+
+		if (index != -1 && this.element) {
+			const sliderElement = this.element.querySelector('.menu-strip__slider');
+			const element = sliderElement.children.item(index);
+
+			if (element) {
+				const left = element.offsetLeft;
+				const width = element.clientWidth;
+
+				this.element.scrollLeft = left - width;
+			}
+		}
 	},
 });
+
