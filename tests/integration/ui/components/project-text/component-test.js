d550b25d6609d011d21aaa6a1d9d146e6fa8278d
Florian Pichler <f.pichler@anfe.ma>
2017-10-14 17:22:39 +0200

    FEATURE: Project Cover component
    Re #156

--- tests/integration/ui/components/project-text/component-test.js
+++ tests/integration/ui/components/project-text/component-test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
 
 describe('Integration | Component | project text', function() {
 	setupComponentTest('project-text', {
@@ -9,36 +10,18 @@ describe('Integration | Component | project text', function() {
 	});
 
 	const data = {
-		title: 'foo',
-		subtitle: 'bar',
-		header: 'baz',
-		text: 'qux',
+		content: 'foo',
 	};
 
 	beforeEach(function() {
 		this.set('data', data);
-		this.render(hbs`
-			{{project-text data=data}}
-		`);
 	});
 
 	it('renders', function() {
-		// Set any properties with this.set('myProperty', 'value');
-		// Handle any actions with this.on('myAction', function (val) { ... });
-		// Template block usage:
-		// this.render(hbs`
-		// 	{{#project-text}}
-		// 		template content
-		// 	{{/project-text}}
-		// `);
+		this.render(hbs`
+			{{project-text data=data}}
+		`);
 
-		expect(
-			find('.project-text__title')
-				.innerText.toLowerCase()
-				.trim()
-		).to.equal('foo');
-		expect(find('.project-text__subtitle').innerText.trim()).to.equal('bar');
-		expect(find('.project-text__header').innerText.trim()).to.equal('baz');
-		expect(find('.project-text__text').innerText.trim()).to.equal('qux');
+		expect(find('.project-text').innerText).to.contain(data.content);
 	});
 });
