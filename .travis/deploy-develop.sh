271df48df622ee2780bb9eb9b3b5e160fea0b7d3
Florian Pichler <f.pichler@anfe.ma>
2017-10-16 17:54:06 +0200

    BUGFIX: Adjusted build script
    Re #161

--- .travis/deploy-develop.sh
+++ .travis/deploy-develop.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 DEPLOY_TIMESTAMP=$(date +%Y-%m-%dT%H-%I-%S)
-DEPLOY_PAYLOAD="../$DEPLOY_TIMESTAMP.tgz"
+DEPLOY_PAYLOAD="$DEPLOY_TIMESTAMP.tgz"
 
 cd $TRAVIS_BUILD_DIR
 
 echo "Building Ember Application"
-#ember build --env=production || exit 1
+ember build --env=production || exit 1
 
 echo "Installing FastBoot packages"
-pushd dist
-	yarn install  || exit 1
-popd
+pushd dist || exit 1
+	yarn install || exit 1
 
-echo "Create dist archive at $DEPLOY_PAYLOAD"
-cd dist
-tar -czf $DEPLOY_PAYLOAD . || exit 1
+	echo "Create dist archive at $DEPLOY_PAYLOAD"
+	tar -czf ../$DEPLOY_PAYLOAD . || exit 1
+popd
 
 echo "Copy payload to server"
 scp $DEPLOY_PAYLOAD homepage@banana.anfema.net:/home/homepage/site/ || exit 1
