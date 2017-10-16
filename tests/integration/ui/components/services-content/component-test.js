4355a293aee866678e6fc0a26ba6a2824b0300c3
Philipp Grieser <philippgrieser@gmail.com>
2017-10-16 12:03:44 +0200

    BUG: Language-selector does not work in projects/detail route
    Change link-to helper to a element with transition defined in action onLinkClick
    
    Re #127

--- tests/integration/ui/components/services-content/component-test.js
+++ tests/integration/ui/components/services-content/component-test.js
@@ -46,14 +46,6 @@ describe('Integration | Component | services content', function() {
 	});
 
 	it('renders', function() {
-		// Set any properties with this.set('myProperty', 'value');
-		// Handle any actions with this.on('myAction', function (val) { ... });
-		// Template block usage:
-		// this.render(hbs`
-		// 	{{#services-content}}
-		// 		template content
-		// 	{{/services-content}}
-		// `);
 		this.set('services', services);
 		this.render(hbs`{{services-content data=services}}`);
 		expect(find('.service-content')).to.exist;
