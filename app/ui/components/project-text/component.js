69c39c1cdf5cace3019f9d4718da87c9ea33078a
radofo <r.adofo@anfe.ma>
2018-01-09 10:54:02 +0100

    BUGFIX: Provide expected whitespaces
    Provide whitespaces in project detail pages as defined in designs
    
    Re #222

--- app/ui/components/project-text/component.js
+++ app/ui/components/project-text/component.js
@@ -4,7 +4,11 @@ import { alias } from '@ember/object/computed';
 export default Component.extend({
 	// data: null,
 
-	classNameBindings: ['useAlternateColor:project-text--alt'],
+	classNameBindings: [
+		'useAlternateColor:project-text--alt',
+		'assignmentText:project-text--assignment',
+	],
 
 	useAlternateColor: alias('data.useAlternateColor'),
+	assignmentText: alias('data.assignmentText'),
 });
