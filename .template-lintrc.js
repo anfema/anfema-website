5cf497f076595d6ed063ab9c1ffb4d33f9f5d67a
Philipp Skorpil <p.skorpil@anfe.ma>
2020-04-20 17:40:56 +0200

    chore - update dependenies
    - update linting-rules
    - fix linting-errors in templates due to newer linter
    
    re #346

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,10 @@
 
 module.exports = {
 	extends: 'recommended',
+	rules: {
+		'no-bare-strings': true,
+		'attribute-indentation': false,
+		'block-indentation': 'tab',
+		'no-triple-curlies': false, // remove with ticket #353
+	},
 };
