fbff3e05e8247559eaefe27e3e64c65311bad2c2
sariita05 <s.eidenhammer@anfe.ma>
2020-09-23 16:20:01 +0200

    Enhancement: Add project top feature component
    Re #370

--- app/ui/components/project-topFeatures-item/component.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-
-export default Component.extend({
-	isActive: computed('data.id', 'selected', function() {
-		return this.get('data.id') === this.selected;
-	}),
-});
