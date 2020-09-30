5d18e7ce0f539c15f30e7921e92b6e4097dba480
sariita05 <s.eidenhammer@anfe.ma>
2020-09-30 10:14:19 +0200

    Enhancement: Add project header component
    Re #369

--- tests/integration/ui/components/project-icons/component-test.js
+++ tests/integration/ui/components/project-icons/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | content-project-icons', function(hooks) {
+module('Integration | Component | project-icons', function(hooks) {
 	setupRenderingTest(hooks);
 
 	test('it renders', async function(assert) {
 		// Set any properties with this.set('myProperty', 'value');
 		// Handle any actions with this.set('myAction', function(val) { ... });
 
-		await render(hbs`{{content-project-icons}}`);
+		await render(hbs`{{project-icons}}`);
 
 		assert.equal(this.element.textContent.trim(), '');
 
 		// Template block usage:
 		await render(hbs`
-      {{#content-project-icons}}
+      {{#project-icons}}
         template block text
-      {{/content-project-icons}}
+      {{/project-icons}}
     `);
 
 		assert.equal(this.element.textContent.trim(), 'template block text');
