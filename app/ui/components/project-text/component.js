448f02bc72650ba3da87d6404940dfe64fdb63d7
sariita05 <s.eidenhammer@anfe.ma>
2020-09-30 16:31:55 +0200

    Enhancement: Add project header component Fixed: Stylings and Classes Re #369

--- app/ui/components/project-text/component.js
+++ app/ui/components/project-text/component.js
@@ -1,14 +1,3 @@
 import Component from '@ember/component';
-import { alias } from '@ember/object/computed';
 
-export default Component.extend({
-	// data: null,
-
-	classNameBindings: [
-		'useAlternateColor:project-text--alt',
-		'assignmentText:project-text--assignment',
-	],
-
-	useAlternateColor: alias('data.useAlternateColor'),
-	assignmentText: alias('data.assignmentText'),
-});
+export default Component.extend({});
