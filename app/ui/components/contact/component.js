0ff392a599e512308bbd9c44ab75639585951117
Philipp Skorpil <p.skorpil@anfe.ma>
2020-11-09 14:13:53 +0100

    fix mailto-link in page-footer component #385 \nRe #385

--- app/ui/components/contact/component.js
+++ app/ui/components/contact/component.js
@@ -1,3 +1,26 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default Component.extend({});
+export default Component.extend({
+	intl: service(),
+
+	createMailToLinkWithContent(email, subject, body) {
+		const subjectEncoded = encodeURIComponent(subject).trim();
+		const bodyEncoded = encodeURIComponent(body).trim();
+
+		return `mailto:${email}?subject=${subjectEncoded}&body=${bodyEncoded}`;
+	},
+
+	mailtoLink: computed('data', 'meta', function() {
+		const data = this.get('data');
+		const meta = this.get('meta');
+		const email = this.intl.t('component.page-footer.email.value');
+		const subject = `${data.subject} ${meta.client} ${meta.title}`;
+		const body = data.body.replace('{{projectInfo}}', `${meta.client} ${meta.title}`);
+
+		console.log(data.body.partTwo);
+
+		return this.createMailToLinkWithContent(email, subject, body);
+	}),
+});
