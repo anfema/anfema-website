75945958f7be92cd3700c26ca4d9a344236470d3
Sven ten Pas <s.tenpas@anfe.ma>
2021-01-13 19:01:50 +0100

    Fix linter errors
    Re #437

--- app/helpers/equal.js
+++ app/helpers/equal.js
@@ -1,4 +1,5 @@
 import { helper } from '@ember/component/helper';
+
 export function equal(params) {
 	return params[0] === params[1];
 }
--- app/ui/components/content-lp-team/component.js
+++ app/ui/components/content-lp-team/component.js
@@ -1,10 +1,6 @@
 import Component from '@ember/component';
-import { action } from '@ember/object';
 
 export default Component.extend({
-	// data: null,
-	// selected: null,
-
 	activeItemIndex: null,
 
 	actions: {
